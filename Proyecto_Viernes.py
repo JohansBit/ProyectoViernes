2a6d1b57b178b1b91aac7c769e1d4211dcea5284
XQodaX <ben.gutierrezm@duocuc.cl>
2025-06-13 21:19:57 -0400

    Esta es una mejora del código para la opción 3

--- Proyecto_Viernes.py
+++ Proyecto_Viernes.py
@@ -1,80 +1,23 @@
-'''rut tiene que ser formato "texto" en un input solo
-   diccionario ejemplo:
-   {rut :[nombre,apellidoPaterno,edad,estadoCivil,sexo, fecha_afiliacion]}'''
-from datetime import date
-from random import randint
-from rutificador import Rut as validar
-personas={}
-sw=0
 
-while sw==0:
-    print("-"*8)
-    print("Bienvenido a Vida y Salud")
-    print("-"*8)
-    print("1.Ingresar usuarios")
-    print("2.Buscar Usuario")
-    print("3.Imprimir Certificado ")
-    print("4.SALIR")
-    try:
-        opcion=int(input("Ingrese opción: "))
-        assert(opcion in range(1,5))
-    except ValueError:
-        print("Debe ingresar una opción válida")
-        continue
-    except AssertionError:
-        print("Debe elegir una opcion: [1-2-3-4]")
-    except Exception as e:
-        print(f"Ha ocurrido un error inesperado: {e}")
-    if opcion==1:
-        while True:
-            try:
-                rut=input("Ingrese su rut: (00000000-0) ")
-                nombre=str(input("Ingrese su Nombre: "))
-                apellidoPaterno=str(input("Ingrese su apellido paterno: "))
-                edad=int(input("Ingrese su edad: "))
-                estadoCivil=str(input("Ingrese su estado civil: [Soltero: S, Casado: C , Viudo: V] ")).upper().strip()
-                sexo=str(input("Ingrese su género: [Masculino - Femenino] "))
-                fecha_afiliacion=input("Ingrese la fecha de afiliacion (dd/mm/aaaa): ")
-                assert(edad>=18)
-            except ValueError:
-                print("Ha ingresado informacion incorrecta ")
-                continue
-            except AssertionError:
-                print("La edad debe ser mayor o igual a 18")
-                continue
-            except Exception as e:
-                print("Ha ocurrido un error: ",e)
-                continue
-            if validar(rut):
-                print(f"El rut {rut} es válido")
-            else:
-                print("Rut inválido")
-                continue
-            if estadoCivil=="C":
-                estadoCivil="Casado"
-            elif estadoCivil=="S":
-                estadoCivil="Soltero"
-            elif estadoCivil=="V":
-                estadoCivil="Viudo"
-            else:
-                print("Debe ingresar una opcion valida para su estado civil")
-                continue
-            
-                
-            personas={rut:{"Nombre": nombre, "Apellido": apellidoPaterno,
-                            "edad":edad, "estadoCivil": estadoCivil, "Sexo":sexo, "fecha_afiliacion": fecha_afiliacion}}
-            print(personas[rut]["Nombre"])
-            break
 
-          
-    elif opcion == "4":
-        print("\n*******************************")
-        print("\nGracias por usar el registro de afiliados de Isapre VIDA Y SALUD")
-        print("\nIntegrantes: Nicolás Fernandez \nBenjamín Gutierrez \nJavier Valtierra \nJohans Sepulveda")
-        print("\n Python:3.1 ")
-        print("\n*******************************")
-        break
+
+if opcion == 3:
+
+    fecha = date.today()
+    valor_certificado = random.randint(1000,1500)
+
+    if sexo == "masculino":
+        genero = "el señor"
+    elif sexo == "femenino":
+        genero = "la señorita"
+
+    solicitud = input("Ingrese el rut que desea solicitar el certificado: ")
+
+    if solicitud in personas:
+      print("*****Certificado de afiliación*****")
+      print(f"Con fecha {fecha}, la persona " ,personas[solicitud]["nombre"] ,personas[solicitud]["apellidoPaterno"] ,", a la edad de ",personas[solicitud]["edad"]," / "personas[solicitud]["esatdoCivil"], ", a la edad de ",personas[solicitud]["edad"]" por un precio de ${valor_certificado} queda asociado a la isapre *Vida y Salud*")
     else:
-        print("\nOpcion invalida porfavor reintente ")
-    
+      print("No se encuentra el rut indicado")
 
+
+      
